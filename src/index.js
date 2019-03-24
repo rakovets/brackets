ede79c2fbaf1cade4265e8277ed9316dd564c034
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-03-24 23:26:13 +0300

    feat: impl brackets validation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stringForValidation = str;
+  for (let i = 0; i < stringForValidation.length; i++) {
+    for (let configSet = 0; configSet < bracketsConfig.length; configSet++) {
+      if ((stringForValidation[i] === bracketsConfig[configSet][0])
+        && (stringForValidation[i + 1] === bracketsConfig[configSet][1])) {
+
+        let reg = bracketsConfig[configSet][0] + bracketsConfig[configSet][1];
+        stringForValidation = stringForValidation.replace(reg, '');
+        configSet = -1;
+        i--;
+      }
+    }
+  }
+  return stringForValidation.length == 0;
 }
